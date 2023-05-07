f36c95be414222aac8c12077f5c154af59d306a9
Febin.Jose <Febin.Jose@sky.co.nz>
2023-05-07 20:57:26 +1200

    Admin page with custom user details

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,3 +36,11 @@ class ModelTests(TestCase):
         """Creating an user without email raises an exception"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com', 'test123'
+            )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
